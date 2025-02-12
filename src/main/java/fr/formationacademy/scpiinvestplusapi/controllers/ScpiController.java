8e9c933bc68236de6d548311a335977ff2f12f44
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-12 17:08:22 +0100

    Modification of the crossOrigin and removed the autowired annotaion

--- src/main/java/fr/formationacademy/scpiinvestplusapi/controllers/ScpiController.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/controllers/ScpiController.java
@@ -4,20 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 import fr.formationacademy.scpiinvestplusapi.dto.DetailsDTO;
-import fr.formationacademy.scpiinvestplusapi.entity.Scpi;
+
 import fr.formationacademy.scpiinvestplusapi.services.ScpiService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Scpi", description = "API for Scpi ")
 @RequestMapping(path = "api/v1/application/Scpi")
 public class ScpiController {
@@ -32,11 +31,11 @@ public class ScpiController {
         description = "Get all the details from a given SCPI with a specific id as parameter.",
         responses = {
             @ApiResponse(responseCode = "200", description = "An Details object containing different inforamtion." , useReturnTypeSchema = true),
-            @ApiResponse(responseCode = "500", description = "The products where not loaded succefully." , useReturnTypeSchema = true), 
+            @ApiResponse(responseCode = "500", description = "Could not find the details of the selected scpi." , useReturnTypeSchema = true), 
         }    
     )
     @GetMapping("/details/{scpid}")
-    public ResponseEntity<DetailsDTO> getMethodName(@PathVariable int scpid) {
+    public ResponseEntity<DetailsDTO> getDetailsById(@PathVariable int scpid) {
         return ResponseEntity.ok( this.scpiService.gettAllDetails( scpid));
     }
     
