f996baace3d56e9e7edd6daccaf7aced6e98836e
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-14 22:46:06 +0100

    modification from code review

--- src/main/java/fr/formationacademy/scpiinvestplusapi/dto/ScpiDTO.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/dto/ScpiDTO.java
@@ -1,18 +1,17 @@
 package fr.formationacademy.scpiinvestplusapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.List;
 
-import lombok.*;
-
-/**
- * Class for converting the data gotten from the database for Scpi detail's
- */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ScpiDTO implements Serializable{
+public class ScpiDTO implements Serializable {
 
     private Integer id;
     private String name;
@@ -25,15 +24,12 @@ public class ScpiDTO implements Serializable{
     private String iban;
     private String bic;
     private Boolean scheduledPayment;
-
     private String frequencyPayment;
     private Float cashback;
     private String advertising;
     private List<StatYearDTO> statYears;
     private List<LocationDTO> locations;
     private List<SectorDTO> sectors;
-
-
 
 
 }
