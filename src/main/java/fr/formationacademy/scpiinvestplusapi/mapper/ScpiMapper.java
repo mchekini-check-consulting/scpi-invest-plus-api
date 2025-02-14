f052b4fecb4d4a88fbd466ada0d5a77012980f37
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-14 22:46:06 +0100

    modification from code review

--- src/main/java/fr/formationacademy/scpiinvestplusapi/mapper/ScpiMapper.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/mapper/ScpiMapper.java
@@ -4,7 +4,6 @@ package fr.formationacademy.scpiinvestplusapi.mapper;
 import fr.formationacademy.scpiinvestplusapi.dto.ScpiDTO;
 import fr.formationacademy.scpiinvestplusapi.entity.Scpi;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ScpiMapper {
