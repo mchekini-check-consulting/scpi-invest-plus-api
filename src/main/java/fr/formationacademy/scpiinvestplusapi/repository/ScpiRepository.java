00fbd6048d451b6272bf737f115d62d15519be2f
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-14 22:42:40 +0100

    Delete the unused imports

--- src/main/java/fr/formationacademy/scpiinvestplusapi/repository/ScpiRepository.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/repository/ScpiRepository.java
@@ -2,6 +2,8 @@ package fr.formationacademy.scpiinvestplusapi.repository;
 
 import fr.formationacademy.scpiinvestplusapi.entity.Scpi;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
