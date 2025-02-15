d89050a9bb01b839eea455db02b1a9870138c0a0
SpeedsKillsx <amayaslabchri88@gmail.com>
2025-02-15 23:25:55 +0100

    Added swagger documentation

--- src/main/java/fr/formationacademy/scpiinvestplusapi/resource/ScpiResource.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/resource/ScpiResource.java
@@ -3,6 +3,10 @@ package fr.formationacademy.scpiinvestplusapi.resource;
 import fr.formationacademy.scpiinvestplusapi.dto.ScpiDtoOut;
 import fr.formationacademy.scpiinvestplusapi.service.ScpiService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +29,15 @@ public class ScpiResource {
     public ResponseEntity<List<ScpiDtoOut>> getScpi() {
         return ResponseEntity.ok(scpiService.getScpis());
     }
-
+    @Operation(summary = "Retrieve SCPI details by ID",
+            description = "Returns SCPI details as a DTO based on the provided ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success - SCPI details retrieved",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ScpiDtoOut.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "SCPI not found")
+    })
     @GetMapping("/details/{scpid}")
     public ResponseEntity<ScpiDtoOut> getDetailsById(@PathVariable Integer scpid) {
         return ResponseEntity.ok(scpiService.getScpiDetailsById(scpid));
