64de4efc00b9595b98950c8eff95f1c1fd658818
kouci <56268526+kouci@users.noreply.github.com>
2025-02-16 16:18:30 +0100

    Feat/us 21 8 20 19 6 investor scpi (#16)
    features:us_19_8_21_19_investor_scpi

--- src/main/java/fr/formationacademy/scpiinvestplusapi/repository/InvestorRepository.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/repository/InvestorRepository.java
@@ -4,6 +4,11 @@ import fr.formationacademy.scpiinvestplusapi.entity.Investor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InvestorRepository extends JpaRepository<Investor, String> {
+
+    Optional<Investor> findByEmail(String email);
+
 }
