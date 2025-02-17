d61b0b5ea4fc548a57fef04adc6c93e21589baa4
nasseraitahmed07 <salimnasserait@gmail.com>
2025-02-17 18:45:22 +0100

    fix unique key bic issue

--- src/main/resources/db/migration/V0.0.3__update-bic_unique.sql
+++ src/main/resources/db/migration/V0.0.3__update-bic_unique.sql
@@ -1,7 +1,7 @@
 CREATE SEQUENCE IF NOT EXISTS scpi_id_seq;
 ALTER TABLE scpi ALTER COLUMN id SET DEFAULT nextval('scpi_id_seq');
 
-ALTER TABLE scpi DROP CONSTRAINT scpi_bic_key;
+ALTER TABLE scpi DROP CONSTRAINT IF EXISTS scpi_bic_key;
 
 
 
