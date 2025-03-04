8fe9a3813db1e208853394a49998682930b3f40a
nasseraitahmed07 <salimnasserait@gmail.com>
2025-03-04 01:55:15 +0100

    wip

--- src/main/java/fr/formationacademy/scpiinvestplusapi/repository/ScpiRepository.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/repository/ScpiRepository.java
@@ -38,5 +38,4 @@ ScpiRepository extends JpaRepository<Scpi, Integer> {
     @Query("SELECT s FROM Scpi s WHERE s.name IN :names")
     Set<Scpi> findByNameIn(@Param("names") List<String> names);
 
-    Optional<Scpi> findByIban(@Pattern(regexp = IBAN_PATTERN , message = INVALID_IBAN) String iban);
 }
