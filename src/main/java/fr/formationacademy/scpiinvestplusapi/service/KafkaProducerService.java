4fa7a7269013daaa2edd2e671db0a2af86c16920
speedkillsx <amayaslabchri88@gmail.com>
2025-03-11 02:43:59 +0100

    Add the Producer for the communication between the application and partners

---
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/service/KafkaProducerService.java
@@ -0,0 +1,22 @@
+package fr.formationacademy.scpiinvestplusapi.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducerService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String topic, String message) {
+        log.info("Sending message to consumers: " + message);
+        kafkaTemplate.send(topic, message);
+    }
+}
