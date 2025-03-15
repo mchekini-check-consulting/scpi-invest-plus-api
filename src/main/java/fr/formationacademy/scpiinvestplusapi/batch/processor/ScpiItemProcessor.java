06def07c112c9f093dd58736c5217fb90080f13d
nasseraitahmed07 <salimnasserait@gmail.com>
2025-03-15 23:56:35 +0100

    batch an plan logging simplified

--- src/main/java/fr/formationacademy/scpiinvestplusapi/batch/processor/ScpiItemProcessor.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/batch/processor/ScpiItemProcessor.java
@@ -9,7 +9,6 @@ import fr.formationacademy.scpiinvestplusapi.entity.Sector;
 import fr.formationacademy.scpiinvestplusapi.entity.StatYear;
 import fr.formationacademy.scpiinvestplusapi.repository.ScpiRepository;
 import fr.formationacademy.scpiinvestplusapi.service.LocationService;
-import fr.formationacademy.scpiinvestplusapi.service.ScpiService;
 import fr.formationacademy.scpiinvestplusapi.service.SectorService;
 import fr.formationacademy.scpiinvestplusapi.service.StatYearService;
 import io.micrometer.common.lang.NonNull;
@@ -30,7 +29,6 @@ public class ScpiItemProcessor implements ItemProcessor<BatchDataDto, Scpi> {
     private final LocationService locationService;
     private final SectorService sectorService;
     private final StatYearService statYearService;
-    private final ScpiService scpiService;
 
     public final Map<String, Scpi> existingScpis = new HashMap<>();
     private final Set<String> scpisInCsv = new HashSet<>();
@@ -45,7 +43,7 @@ public class ScpiItemProcessor implements ItemProcessor<BatchDataDto, Scpi> {
     }
 
     public void refreshCache() {
-        log.info("Rechargement du cache des SCPIs...");
+        log.debug("Rechargement du cache des SCPIs...");
         List<Scpi> scpis = scpiRepository.findAll();
         scpis.forEach(scpi -> existingScpis.put(scpi.getName(), scpi));
     }
@@ -58,7 +56,6 @@ public class ScpiItemProcessor implements ItemProcessor<BatchDataDto, Scpi> {
             return null;
         }
         scpisInCsv.add(dto.getName());
-
 
         Scpi existingScpi = existingScpis.get(dto.getName());
 
@@ -118,7 +115,6 @@ public class ScpiItemProcessor implements ItemProcessor<BatchDataDto, Scpi> {
     public boolean isSame(Scpi existing, ScpiDto dto) {
         return Objects.equals(existing.getMinimumSubscription(), dto.getMinimumSubscription()) && Objects.equals(existing.getCapitalization(), dto.getCapitalization()) && Objects.equals(existing.getManager(), dto.getManager()) && Objects.equals(existing.getSubscriptionFees(), dto.getSubscriptionFees()) && Objects.equals(existing.getManagementCosts(), dto.getManagementCosts()) && Objects.equals(existing.getEnjoymentDelay(), dto.getEnjoymentDelay()) && Objects.equals(existing.getIban(), dto.getIban()) && Objects.equals(existing.getBic(), dto.getBic()) && Objects.equals(existing.getScheduledPayment(), dto.getScheduledPayment()) && Objects.equals(existing.getFrequencyPayment(), dto.getFrequencyPayment()) && Objects.equals(existing.getCashback(), dto.getCashback()) && Objects.equals(existing.getAdvertising(), dto.getAdvertising()) && Objects.equals(existing.getLocations(), dto.getLocations()) && Objects.equals(existing.getStatYears(), dto.getStatYear()) && Objects.equals(existing.getSectors(), dto.getSectors());
     }
-
 
 }
 
--- src/main/java/fr/formationacademy/scpiinvestplusapi/service/PlanService.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/service/PlanService.java
@@ -3,13 +3,11 @@ package fr.formationacademy.scpiinvestplusapi.service;
 import fr.formationacademy.scpiinvestplusapi.dto.PlanDto;
 import fr.formationacademy.scpiinvestplusapi.mapper.PlanMapper;
 import fr.formationacademy.scpiinvestplusapi.repository.PlanRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@Slf4j
 public class PlanService {
 
     private final PlanRepository planRepository;
@@ -22,7 +20,6 @@ public class PlanService {
 
     public List<PlanDto> getAllPlans() {
         List<PlanDto> plans = planRepository.findAll().stream().map(planMapper::toPlanDto).toList();
-        log.info("Fetched all plans successfully. Total plans: {}", plans.size());
         return plans;
     }
 }
