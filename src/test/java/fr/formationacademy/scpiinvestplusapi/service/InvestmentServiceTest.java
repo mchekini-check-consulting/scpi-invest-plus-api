0877a38c17e2fd23ccf6eb78d954653dddacceb6
Amayas Labchri <77687759+SpeedKillsx@users.noreply.github.com>
2025-03-17 16:04:27 +0100

    Update test File in Investment (#60)

--- src/test/java/fr/formationacademy/scpiinvestplusapi/service/InvestmentServiceTest.java
+++ src/test/java/fr/formationacademy/scpiinvestplusapi/service/InvestmentServiceTest.java
@@ -1,26 +1,31 @@
 package fr.formationacademy.scpiinvestplusapi.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import fr.formationacademy.scpiinvestplusapi.dto.InvestmentDto;
+import fr.formationacademy.scpiinvestplusapi.dto.InvestmentOutDto;
+import fr.formationacademy.scpiinvestplusapi.dto.ScpiDtoOut;
 import fr.formationacademy.scpiinvestplusapi.entity.Investment;
+import fr.formationacademy.scpiinvestplusapi.entity.Investor;
 import fr.formationacademy.scpiinvestplusapi.entity.Scpi;
 import fr.formationacademy.scpiinvestplusapi.globalExceptionHandler.GlobalException;
 import fr.formationacademy.scpiinvestplusapi.mapper.InvestmentMapper;
 import fr.formationacademy.scpiinvestplusapi.repository.InvestmentRepository;
 import fr.formationacademy.scpiinvestplusapi.repository.ScpiRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
+@Slf4j
 class InvestmentServiceTest {
 
     @Mock
@@ -28,19 +33,22 @@ class InvestmentServiceTest {
 
     @Mock
     private ScpiRepository scpiRepository;
-
+    @Mock
+    private ScpiService scpiService;
     @Mock
     private InvestmentMapper investmentMapper;
 
     @Mock
     private UserService userService;
-
+    @Mock
+    private KafkaTemplate<String, Object> kafkaTemplate;
     @InjectMocks
     private InvestmentService investmentService;
 
     private Investment investment;
     private InvestmentDto investmentDto;
     private Scpi scpi;
+    private Investor investor;
 
     @BeforeEach
     void setUp() {
@@ -63,15 +71,24 @@ class InvestmentServiceTest {
         investmentDto.setInvestmentState("En cours");
         investmentDto.setTotalAmount(new BigDecimal("5000"));
         investmentDto.setScpiId(1);
+        ScpiDtoOut scpiDtoOut = new ScpiDtoOut();
+        scpiDtoOut.setId(1);
+        scpiDtoOut.setName("SCPI Test");
+        when(scpiService.getScpiDetailsById(1)).thenReturn(scpiDtoOut);
+
+        InvestmentOutDto investmentOutDto = new InvestmentOutDto();
+        investmentOutDto.setId(1);
+        when(investmentMapper.toOutDto(any(Investment.class))).thenReturn(investmentOutDto);
+
     }
 
     @Test
-    void shouldSaveInvestmentSuccessfully() throws GlobalException, JsonProcessingException {
+    void shouldSaveInvestmentSuccessfully() throws GlobalException {
         when(investmentMapper.toEntity(investmentDto)).thenReturn(investment);
         when(scpiRepository.findById(1)).thenReturn(java.util.Optional.of(scpi));
         when(investmentRepository.save(any(Investment.class))).thenReturn(investment);
         when(investmentMapper.toDTO(any(Investment.class))).thenReturn(investmentDto);
-
+        
         InvestmentDto result = investmentService.saveInvestment(investmentDto);
 
         assertThat(result).isNotNull();
