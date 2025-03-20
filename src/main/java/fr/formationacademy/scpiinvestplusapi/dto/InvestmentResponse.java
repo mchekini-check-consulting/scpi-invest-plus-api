d60f5699b29eb3e9a0977c92240f1d2aa6d90c9c
nasseraitahmed07 <salimnasserait@gmail.com>
2025-03-20 14:26:18 +0100

    wip

---
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/dto/InvestmentResponse.java
@@ -0,0 +1,23 @@
+package fr.formationacademy.scpiinvestplusapi.dto;
+
+import fr.formationacademy.scpiinvestplusapi.enums.InvestmentState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InvestmentResponse {
+    private InvestmentState investmentState;
+    private String investorEmail;
+    private String scpiName;
+    private Integer investmentId;
+    private BigDecimal amount;
+    private String rejectionReason;
+}
+
--- src/main/java/fr/formationacademy/scpiinvestplusapi/eventListner/InvestmentResponseListener.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/eventListner/InvestmentResponseListener.java
@@ -1,15 +1,11 @@
 package fr.formationacademy.scpiinvestplusapi.eventListner;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import fr.formationacademy.scpiinvestplusapi.dto.InvestmentResponse;
+import fr.formationacademy.scpiinvestplusapi.enums.InvestmentState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 import static fr.formationacademy.scpiinvestplusapi.utils.Constants.SCPI_PARTNER_GROUP;
 import static fr.formationacademy.scpiinvestplusapi.utils.Constants.SCPI_PARTNER_RESPONSE_TOPIC;
@@ -19,23 +15,24 @@ public class InvestmentResponseListener {
     private static final Logger logger = LoggerFactory.getLogger(InvestmentResponseListener.class);
 
     @KafkaListener(topics = SCPI_PARTNER_RESPONSE_TOPIC, groupId = SCPI_PARTNER_GROUP)
-    public void consumeResponse(ConsumerRecord<String, String> record) {
-        logger.info("Received investment response: {}", record);
-        String jsonResponse = record.value();
+    public void consumeResponse(InvestmentResponse response) {
+        logger.info("Received investment response: {}", response);
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, Object> response = objectMapper.readValue(jsonResponse, new TypeReference<Map<String, Object>>() {});
-            logger.info("Réponse de la demande d'investissement reçu du topic {} : {}", SCPI_PARTNER_RESPONSE_TOPIC, response);
-            processResponse(response);
-        } catch (JsonProcessingException e) {
-            logger.error("Erreur de parsing du message Kafka : {}", e.getMessage(), e);
+            if (response != null) {
+                logger.info("Réponse de la demande d'investissement reçu : {}", response);
+                processResponse(response);
+            } else {
+                logger.error("Message Kafka vide ou mal formé");
+            }
+        } catch (Exception e) {
+            logger.error("Erreur de traitement du message Kafka : {}", e.getMessage(), e);
         }
     }
 
-    private void processResponse(Map<String, Object> response) {
-        String status = (String) response.get("status");
-        String investorEmail = (String) response.get("investorEmail");
-        String scpiName = (String) response.get("scpiName");
+    private void processResponse(InvestmentResponse response) {
+        InvestmentState status = response.getInvestmentState();
+        String investorEmail = response.getInvestorEmail();
+        String scpiName = response.getScpiName();
         logger.info("Traitement du message : Status={}, InvestorEmail={}, ScpiName={}", status, investorEmail, scpiName);
     }
 }
