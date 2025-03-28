03fd3aa7f74a6f91eb20dfabdcd0a290f7adedd5
speedkillsx <amayaslabchri88@gmail.com>
2025-03-28 13:57:52 +0100

    wip

--- src/main/java/fr/formationacademy/scpiinvestplusapi/entity/ScpiIndex.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/entity/ScpiIndex.java
@@ -1,12 +1,18 @@
 package fr.formationacademy.scpiinvestplusapi.entity;
 
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,5 +24,19 @@ public class ScpiIndex {
     @Id
     private String id;
     private String name;
+    private Integer minimumSubscription;
+    private String manager;
+    private Long capitalization;
+    private BigDecimal subscriptionFees;
+    private BigDecimal managementCosts;
+    private Integer enjoymentDelay;
+    private String frequencyPayment;
+    private Float cashback;
+    private String advertising;
+    @OneToMany(mappedBy = "scpi", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Location> locations;
+
+    @OneToMany(mappedBy = "scpi", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Sector> sectors;
 
 }
