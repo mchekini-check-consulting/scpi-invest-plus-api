c915f250ba817179cd88aa28d1a4ab865fed1182
kouci <mansourakociela@gmail.com>
2025-04-09 22:54:07 +0200

    chore(db): remove deprecated Flyway migration version

--- src/main/resources/db/migration/V0.1.2__set_default_investment_status_and_insert_plan.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE IF EXISTS investment
-    ADD COLUMN IF NOT EXISTS status VARCHAR(50) DEFAULT 'STANDARD';
-
-INSERT INTO plan (functionality, "standard", premium, description)
-VALUES ('scheduled-payment', FALSE, TRUE, 'scheduled payment');
