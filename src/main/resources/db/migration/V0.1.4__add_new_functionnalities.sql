e9131fe4e45fcd3510f91676fc25b31ab34322bc
mahdi chekini <mchekini@checkconsulting.fr>
2025-04-12 02:18:47 +0200

    Add new functionnalities into plan table

---
+++ src/main/resources/db/migration/V0.1.4__add_new_functionnalities.sql
@@ -0,0 +1,5 @@
+INSERT INTO plan (functionality, standard, premium, description) VALUES
+                 ('credit', TRUE, TRUE, 'Simulateur de crédit immobilier'),
+                 ('plans', TRUE, TRUE, 'Choix de l''abonnement'),
+                 ('profile', TRUE, TRUE, 'Détermine les informations du profil connecté'),
+                 ('scheduled-payment', TRUE, TRUE, 'Permet de faire des versmenets programmés')
