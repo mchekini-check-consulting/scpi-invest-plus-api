7ab170c73141a25cff82a2de0172a4300e8d52b1
TegHocine <61586802+TegHocine@users.noreply.github.com>
2025-04-22 20:01:30 +0100

    Refactor keycloak webhook to register all user information

--- src/main/java/fr/formationacademy/scpiinvestplusapi/service/KeycloakAdminService.java
+++ src/main/java/fr/formationacademy/scpiinvestplusapi/service/KeycloakAdminService.java
@@ -1,21 +1,30 @@
 package fr.formationacademy.scpiinvestplusapi.service;
 
 import fr.formationacademy.scpiinvestplusapi.configuration.KeycloakConfiguration;
+import fr.formationacademy.scpiinvestplusapi.dto.KeycloakUserDto;
+import fr.formationacademy.scpiinvestplusapi.globalExceptionHandler.GlobalException;
 import jakarta.annotation.PostConstruct;
-
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @Service
+@EnableRetry
 public class KeycloakAdminService {
 
     private final UserService userService;
-    private Keycloak keycloak;
     private final KeycloakConfiguration keycloakConfiguration;
+    private Keycloak keycloak;
 
     public KeycloakAdminService(UserService userService, KeycloakConfiguration keycloakConfiguration) {
         this.userService = userService;
@@ -47,5 +56,34 @@ public class KeycloakAdminService {
 
     }
 
+    @Retryable(
+            retryFor = {GlobalException.class},
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 30000)
+    )
+    public KeycloakUserDto getUserFromKeycloak(String userId) throws GlobalException {
+        try {
+            UserRepresentation userRepresentation = keycloak.realm(keycloakConfiguration.getRealm()).users().get(userId).toRepresentation();
+            Map<String, List<String>> attributes = userRepresentation.getAttributes();
+
+            return KeycloakUserDto.builder()
+                    .email(userRepresentation.getEmail())
+                    .firstName(userRepresentation.getFirstName())
+                    .lastName(userRepresentation.getLastName())
+                    .phoneNumber(getSingleAttribute(attributes, "phoneNumber"))
+                    .dateOfBirth(LocalDate.parse(getSingleAttribute(attributes, "dateOfBirth")))
+                    .maritalStatus(getSingleAttribute(attributes, "maritalStatus"))
+                    .numberOfChildren(getSingleAttribute(attributes, "numberOfChildren"))
+                    .annualIncome(Integer.valueOf(getSingleAttribute(attributes, "annualIncome")))
+                    .build();
+
+        } catch (Exception e) {
+            throw new GlobalException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    private String getSingleAttribute(Map<String, List<String>> attributes, String key) {
+        return attributes.getOrDefault(key, List.of("")).stream().findFirst().orElse("");
+    }
 }
 
